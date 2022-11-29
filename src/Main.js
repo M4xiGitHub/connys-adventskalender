16ac9bbcfca3430b07435e50ce3ef467acd304f1
Maximilian Bleier <maximilian.bleier75@gmail.com>
2022-11-29 18:37:32 +0100

    Favicon

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,15 @@
 import { Box, Grid, Button, Typography, Zoom } from "@mui/material";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 
 import { Environment } from "@react-three/drei";
+import Door from "./Door";
 import Schleife from "./schleife.png";
 import Present from "./Present";
 import Photo1 from "./images/1.JPG";
-const monthNames = [
+import Data from "./Popup.json";
+const MONTHS = [
     "Jänner",
     "Februar",
     "März",
@@ -22,12 +24,45 @@ const monthNames = [
     "Dezember",
 ];
 
+const PHOTOS = [
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+    Photo1,
+];
+
 export default function Main() {
     var datum = new Date();
     const [open, setOpen] = useState(false);
     const [ui, setUi] = useState(false);
+    const [boxClick, setBoxClick] = useState(false);
+    const [clickedDay, setClickedDay] = useState(1);
 
-    const icon = (
+    useEffect(() => {
+        setOpen(false);
+    }, [boxClick]);
+
+    const popup = (
         <Box
             position={"absolute"}
             left={0}
@@ -39,7 +74,8 @@ export default function Main() {
             boxShadow={10}
             display={"flex"}
             justifyContent={"center"}
-            width={{xs:"90vw",md:"50vw"}}
+            width={{ xs: "90vw", md: "40vw" }}
+            height={{ xs: "82vh", md: "80vh" }}
             borderRadius={4}
             zIndex={1000}
         >
@@ -47,46 +83,41 @@ export default function Main() {
                 m={2}
                 display={"flex"}
                 flexDirection={"column"}
+                width={"100%"}
                 justifyContent={"space-between"}
             >
                 <Box
-                    sx={{ height: {xs: "50vh", md:"50vh"},  objectFit:"cover"}}
+                    sx={{
+                        height: { xs: "50vh", md: "30vh" },
+                        objectFit: "cover",
+                    }}
                     component="img"
                     alt="Photo"
-                    src={Photo1}
+                    src={PHOTOS[clickedDay - 1]}
                     borderRadius={4}
                 />
                 <Box my={2} overflow={"scroll"}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
-                    totam incidunt ipsum magni nobis aperiam magnam suscipit
-                    reprehenderit distinctio ducimus facilis illo dicta doloribus,
-                    neque alias, quis maxime esse. Commodi. icta commodi placeat
-                    molestiae temporibus quidem veritatis hic officia, dolor,
-                    accusantium delectus culpa quod a? Incidunt, obcaecati
-                    exercitationem at illum quis, rerum pariatur architecto eaque
-                    ipsum non vitae?
+                    <Typography style={{ wordWrap: "break-word" }}>
+                        {Data.data[clickedDay - 1].text}
+                    </Typography>
                 </Box>
-                <Button variant={"contained"} onClick={() => {setUi(false)}}>Ok</Button>
+                <Button
+                    variant={"contained"}
+                    onClick={() => {
+                        setUi(false);
+                    }}
+                >
+                    Ok
+                </Button>
             </Box>
         </Box>
     );
 
     return (
-        <Grid
-            container
-            width={"100%"}
-            height={"100vh"}
-            display={"flex"}
-            sx={{ bgcolor: "#ba0127" }}
-        >
-            <Grid item xs={12}>
+        <Box display={"flex"} flexDirection={"column"}>
+            <Box mb={5} onClick={() => setBoxClick(false)}>
                 <Box mt={5} mb={1} display={"flex"} justifyContent={"center"}>
-                    <Typography
-                        fontFamily={"Mountains of Christmas"}
-                        fontWeight={"bold"}
-                        variant={"h5"}
-                        color="white"
-                    >
+                    <Typography variant={"Christmas"} color="white">
                         Conny's Adventskalender
                     </Typography>
                 </Box>
@@ -97,6 +128,7 @@ export default function Main() {
                         backgroundRepeat: "no-repeat",
                         backgroundSize: { xs: "100vw", md: "35vw" },
                         width: "100%",
+                        height: { xs: "10vh", md: "50%" },
                         display: "flex",
                         justifyContent: "center",
                         alignItems: "center",
@@ -104,7 +136,6 @@ export default function Main() {
                     alt="Schleife"
                     display={"flex"}
                     justifyContent={"center"}
-                    mb={6}
                 >
                     <Typography
                         fontFamily={"Mountains of Christmas"}
@@ -113,11 +144,139 @@ export default function Main() {
                         color="white"
                         sx={{ marginBottom: "10px" }}
                     >
-                        {datum.getDate() + ". " + monthNames[datum.getMonth()]}
+                        {datum.getDate() + ". " + MONTHS[datum.getMonth()]}
                     </Typography>
                 </Box>
-            </Grid>
-            <Grid item xs={12}>
+            </Box>
+            <Box display={boxClick ? "none" : "block"} overflow={"scroll"}>
+                <Grid
+                    container={true}
+                    justifyContent={"center"}
+                    height={"60vh"}
+                >
+                    <Door
+                        number={1}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={2}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={3}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={4}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={5}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={6}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={7}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={8}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={9}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={10}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={11}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={12}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={13}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={14}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={15}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={16}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={17}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={18}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={19}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={20}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={21}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={22}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={23}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                    <Door
+                        number={24}
+                        click={setBoxClick}
+                        setClickedDay={setClickedDay}
+                    />
+                </Grid>
+            </Box>
+            <Box display={boxClick ? "block" : "none"} height={"60vh"}>
                 <Canvas
                     shadows
                     dpr={[1, 2]}
@@ -125,37 +284,39 @@ export default function Main() {
                 >
                     <ambientLight intensity={0.2} />
                     <spotLight angle={0} penumbra={1} />
-                    <Present setUi={setUi} open={open} />
-
+                    <Present day={clickedDay} setUi={setUi} open={open} />
                     <Environment preset="city" />
                 </Canvas>
-            </Grid>
-            <Zoom style={{ transitionDuration: ui?"1s":"350ms" }} in={ui}>
-                {icon}
-            </Zoom>
-
-            <Box
-                position={"absolute"}
-                bottom={"2rem"}
-                width={"100vw"}
-                display={"flex"}
-                justifyContent={"center"}
-            >
-                <Button
-                    sx={{
-                        padding: "1rem",
-                        paddingX: "8rem",
-                        borderRadius: "4rem",
-                    }}
-                    variant="contained"
-                    color="success"
-                    onClick={() => {
-                        setOpen(true);
-                    }}
+                <Zoom
+                    style={{ transitionDuration: ui ? "1s" : "350ms" }}
+                    in={ui}
                 >
-                    <Typography variant="h6">Open</Typography>
-                </Button>
+                    {popup}
+                </Zoom>
+
+                <Box
+                    position={"absolute"}
+                    bottom={"2rem"}
+                    width={"100vw"}
+                    display={"flex"}
+                    justifyContent={"center"}
+                >
+                    <Button
+                        sx={{
+                            padding: "1rem",
+                            paddingX: "8rem",
+                            borderRadius: "4rem",
+                            color: "white",
+                        }}
+                        variant="contained"
+                        onClick={() => {
+                            setOpen(true);
+                        }}
+                    >
+                        <Typography variant="h6">{clickedDay} Öffnen</Typography>
+                    </Button>
+                </Box>
             </Box>
-        </Grid>
+        </Box>
     );
 }
--- src/Present.js
+++ src/Present.js
@@ -5,7 +5,7 @@ import { useSpring, animated } from "@react-spring/three";
 import { useGLTF } from "@react-three/drei";
 import { MathUtils } from "three";
 
-export default function Present({ open, setUi }) {
+export default function Present({ open, setUi, day }) {
     const { pos } = useSpring({ pos: open ? [0, 0.3, 0] : [0, 0, 0] });
     const { rot } = useSpring({
         rot: open ? [0, MathUtils.degToRad(180), 0] : [0, 0, 0],
@@ -20,11 +20,39 @@ export default function Present({ open, setUi }) {
             refQuestionMark.current.rotation.y + 0.01;
     });
 
-    //materials.BoxMaterial.color.set("#ff22f0")
+    const colors = [
+        "#f7d3d0",
+        "#fa9f98",
+        "#fa7c73",
+        "#f75b4f",
+        "#f44336",
+        "#e91e63",
+        "#9c27b0",
+        "#673ab7",
+        "#3f51b5",
+        "#2196f3",
+        "#03a9f4",
+        "#00bcd4",
+        "#009688",
+        "#4caf50",
+        "#8bc34a",
+        "#cddc39",
+        "#ffeb3b",
+        "#ffc107",
+        "#ff9800",
+        "#ff5722",
+        "#795548",
+        "#9e9e9e",
+        "#607d8b",
+        "#000000",
+    ];
+    if (day <= 24) {
+        materials.BoxMaterial.color.set(colors[day - 1]);
+    }
 
     return (
         <group
-            position={[0,-0.3,0]}
+            position={[0, -0.3, 0]}
             rotation={[MathUtils.degToRad(35), MathUtils.degToRad(45), 0]}
             scale={3}
         >
@@ -77,7 +105,7 @@ export default function Present({ open, setUi }) {
                 geometry={nodes.Object_1.geometry}
                 material={materials.BlockMaterial}
                 position={pos}
-                onClick={() => setUi(true)}
+                onClick={() => (open ? setUi(true) : null)}
             />
         </group>
     );
